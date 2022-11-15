0ad4a10c04688dc509ee1f949af50810b6ba78d3
Sparrow <dfleites1234@gmail.com>
2022-11-15 15:41:00 -0600

    Consuming Promises

--- starter/script.js
+++ starter/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-/*
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
 
@@ -24,6 +24,8 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
+/*
+
 
 const getCountryAndNeighbour = function (country) {
   // AJAX call country 1
@@ -82,10 +84,28 @@ setTimeout(() => {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Promises and the Fetch API
 
-// const request = new XMLHttpRequest();
-// request.open('GET', `https://restcountries.com/v2/name/${country}`);
-// request.send();
-// console.log(request.responseText);
-
 const request = fetch('https://restcountries.com/v2/name/portugal');
 console.log(request);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Consuming Promises
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
