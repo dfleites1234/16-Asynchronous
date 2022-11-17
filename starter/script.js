1149de829c682c739367db2e7e5547bde220f5bc
Sparrow <dfleites1234@gmail.com>
2022-11-17 14:31:43 -0600

    Building a Simple Promise

--- starter/script.js
+++ starter/script.js
@@ -225,7 +225,6 @@ const whereAmI = function (lat, lng) {
 whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
 
-*/
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // The Event Loop in Practice
@@ -240,3 +239,58 @@ Promise.resolve('Resolved promise 2').then(res => {
 });
 
 console.log('Test end');
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Building a Simple Promise
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN');
+    } else {
+      reject(new Error('You lost your money'));
+    }
+  }, 2000);
+});
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 seconds passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 seconds passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 seconds passed'));
+
+//   setTimeout(() => {
+//     console.log('1 second passed');
+//     setTimeout(() => {
+//       console.log('2 seconds passed');
+//       setTimeout(() => {
+//         console.log('3 seconds passed');
+//         setTimeout(() => {
+//           console.log('4 second passed');
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.error(x));
